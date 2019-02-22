201d9f455b9addcc5340390a2988226b86eab7e8
leahtorres <leah_torres@davincischools.org>
2019-02-22 14:46:36 -0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,138 @@
+
+function nextImage(el){
+	if (el.src.match("four.jpg")){
+		 el.src = "pic1.jpg";
+
+	} else if (el.src.match("pic1.jpg")){
+		 el.src = "pic2.jpg";
+		
+	} else if (el.src.match("pic2.jpg")){
+		 el.src = "pic3.jpg";
+		
+	} else if (el.src.match("pic3.jpg")){
+		el.src = "pic4.jpg";
+		// do nothing
+	} else if (el.src.match("pic4.jpg")){
+		el.src = "pic5.jpg";
+	} else if (el.src.match("pic5.jpg")){
+		el.src = "pic6.jpg";
+	}else if (el.src.match("pic6.jpg")) {
+		el.src = "four.jpg";
+		//do nothing
+	}
+}
+
+function nextImage1(el){
+	if (el.src.match("two.jpg")){
+		 el.src = "pic1.jpg";
+
+	} else if (el.src.match("pic1.jpg")){
+		 el.src = "pic2.jpg";
+		
+	} else if (el.src.match("pic2.jpg")){
+		 el.src = "pic3.jpg";
+		
+	} else if (el.src.match("pic3.jpg")){
+		el.src = "pic4.jpg";
+		// do nothing
+	} else if (el.src.match("pic4.jpg")){
+		el.src = "pic5.jpg";
+	} else if (el.src.match("pic5.jpg")){
+		el.src = "pic6.jpg";
+	}else if (el.src.match("pic6.jpg")) {
+		el.src = "two.jpg";
+		//do nothing
+	}
+}
+
+function nextImage2(el){
+	if (el.src.match("six.jpg")){
+		 el.src = "pic1.jpg";
+
+	} else if (el.src.match("pic1.jpg")){
+		 el.src = "pic2.jpg";
+		
+	} else if (el.src.match("pic2.jpg")){
+		 el.src = "pic3.jpg";
+		
+	} else if (el.src.match("pic3.jpg")){
+		el.src = "pic4.jpg";
+		// do nothing
+	} else if (el.src.match("pic4.jpg")){
+		el.src = "pic5.jpg";
+	} else if (el.src.match("pic5.jpg")){
+		el.src = "pic6.jpg";
+	}else if (el.src.match("pic6.jpg")) {
+		el.src = "six.jpg";
+		//do nothing
+	}
+}
+
+function nextImage3(el){
+	if (el.src.match("nine.jpg")){
+		 el.src = "pic1.jpg";
+
+	} else if (el.src.match("pic1.jpg")){
+		 el.src = "pic2.jpg";
+		
+	} else if (el.src.match("pic2.jpg")){
+		 el.src = "pic3.jpg";
+		
+	} else if (el.src.match("pic3.jpg")){
+		el.src = "pic4.jpg";
+		// do nothing
+	} else if (el.src.match("pic4.jpg")){
+		el.src = "pic5.jpg";
+	} else if (el.src.match("pic5.jpg")){
+		el.src = "pic6.jpg";
+	}else if (el.src.match("pic6.jpg")) {
+		el.src = "nine.jpg";
+		//do nothing
+	}
+}
+
+function nextImage4(el){
+	if (el.src.match("three.jpg")){
+		 el.src = "pic1.jpg";
+
+	} else if (el.src.match("pic1.jpg")){
+		 el.src = "pic2.jpg";
+		
+	} else if (el.src.match("pic2.jpg")){
+		 el.src = "pic3.jpg";
+		
+	} else if (el.src.match("pic3.jpg")){
+		el.src = "pic4.jpg";
+		// do nothing
+	} else if (el.src.match("pic4.jpg")){
+		el.src = "pic5.jpg";
+	} else if (el.src.match("pic5.jpg")){
+		el.src = "pic6.jpg";
+	}else if (el.src.match("pic6.jpg")) {
+		el.src = "three.jpg";
+		//do nothing
+	}
+}
+
+function nextImage5(el){
+	if (el.src.match("five.jpg")){
+		 el.src = "pic1.jpg";
+
+	} else if (el.src.match("pic1.jpg")){
+		 el.src = "pic2.jpg";
+		
+	} else if (el.src.match("pic2.jpg")){
+		 el.src = "pic3.jpg";
+		
+	} else if (el.src.match("pic3.jpg")){
+		el.src = "pic4.jpg";
+		// do nothing
+	} else if (el.src.match("pic4.jpg")){
+		el.src = "pic5.jpg";
+	} else if (el.src.match("pic5.jpg")){
+		el.src = "pic6.jpg";
+	}else if (el.src.match("pic6.jpg")) {
+		el.src = "five.jpg";
+		//do nothing
+	}
+}
